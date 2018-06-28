902f558a14e5eae6035ce925993e46f52beb3f16
Martin Greenwood <martin@wearewbeard.com>
2018-06-28 16:19:51 +0100

    fix

--- templates/story.php
+++ templates/story.php
@@ -18,6 +18,9 @@ get_header();
 				<div class="cell middle">
 					<div class="container">
 						<div class="row">
+							<h1><?php the_title(); ?></h1>
+						</div>
+						<div class="row">
 							<?php
 							while ( have_posts() ) : the_post();
 
