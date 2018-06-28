5b960d87c6feafc09d8ebd5f412aae507e4bf047
Martin Greenwood <martin@wearewbeard.com>
2018-06-28 13:53:29 +0100

    updates

--- templates/contact.php
+++ templates/contact.php
@@ -33,6 +33,63 @@ get_header();
 			
 	</div>
 
+	<div id="bios">
+
+		<div class="content">
+			<div class="table">
+				<div class="cell middle">
+					<div class="container">
+						<div class="row">
+							<div class="person">
+								<div class="pic">
+									<img src="<?php the_field( 'bio_one_image' ); ?>" alt="">
+								</div><!--
+								--><div class="bio">
+									<h3><?php the_field( 'bio_one_name' ); ?></h3>
+									<h4><?php the_field( 'bio_one_position' ); ?></h4>
+									<?php the_field( 'bio_one' ); ?>
+								</div>
+							</div><!--
+							--><div class="person">
+								<div class="pic">
+									<img src="<?php the_field( 'bio_two_image' ); ?>" alt="">
+								</div><!--
+								--><div class="bio">
+									<h3><?php the_field( 'bio_two_name' ); ?></h3>
+									<h4><?php the_field( 'bio_two_position' ); ?></h4>
+									<?php the_field( 'bio_two' ); ?>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+			
+	</div>
+
+
+	<div id="social">
+
+		<div class="content">
+			<div class="table">
+				<div class="cell middle">
+					<div class="container">
+						<div class="row">
+							<ul>
+								<li><a href="<?php the_field( 'facebook' ); ?>" target="_blank" title=""><i class="fab fa-facebook-f"></i></a></li>
+								<li><a href="<?php the_field( 'twitter' ); ?>" target="_blank" title=""><i class="fab fa-twitter"></i></a></li>
+								<li><a href="<?php the_field( 'linkedin' ); ?>" target="_blank" title=""><i class="fab fa-linkedin-in"></i></a></li>
+								<li><a href="<?php the_field( 'instagram' ); ?>" target="_blank" title=""><i class="fab fa-instagram"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+			
+	</div>
+
 
 	<div id="primary" class="content-area">
 
