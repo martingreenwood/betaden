963194c40b679d21f8004c02392d57f30d4ae215
Martin Greenwood <martin@wearewbeard.com>
2018-06-29 11:22:19 +0100

    fix

--- templates/contact.php
+++ templates/contact.php
@@ -14,19 +14,15 @@ get_header();
 	<div id="intro">
 
 		<div class="content">
-			<div class="table">
-				<div class="cell middle">
-					<div class="container">
-						<div class="row">
-							<?php
-							while ( have_posts() ) : the_post();
+			<div class="container">
+				<div class="row">
+					<?php
+					while ( have_posts() ) : the_post();
 
-								get_template_part( 'template-parts/content', 'page' );
+						get_template_part( 'template-parts/content', 'page' );
 
-							endwhile; // End of the loop.
-							?>
-						</div>
-					</div>
+					endwhile; // End of the loop.
+					?>
 				</div>
 			</div>
 		</div>
