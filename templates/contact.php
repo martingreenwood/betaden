119df27139b70fc31165de5cf633003291d5688e
Martin Greenwood <martin@wearewbeard.com>
2018-06-29 11:27:46 +0100

    fix

--- templates/contact.php
+++ templates/contact.php
@@ -32,30 +32,26 @@ get_header();
 	<div id="bios">
 
 		<div class="content">
-			<div class="table">
-				<div class="cell middle">
-					<div class="container">
-						<div class="row">
-							<div class="person">
-								<div class="pic">
-									<div class="element" style="background-image: url(<?php the_field( 'bio_one_image' ); ?>");></div>
-								</div><!--
-								--><div class="bio">
-									<h3><?php the_field( 'bio_one_name' ); ?></h3>
-									<h4><?php the_field( 'bio_one_position' ); ?></h4>
-									<?php the_field( 'bio_one' ); ?>
-								</div>
-							</div><!--
-							--><div class="person">
-								<div class="pic">
-									<div class="element" style="background-image: url(<?php the_field( 'bio_two_image' ); ?>);"></div>
-								</div><!--
-								--><div class="bio">
-									<h3><?php the_field( 'bio_two_name' ); ?></h3>
-									<h4><?php the_field( 'bio_two_position' ); ?></h4>
-									<?php the_field( 'bio_two' ); ?>
-								</div>
-							</div>
+			<div class="container">
+				<div class="row">
+					<div class="person">
+						<div class="pic">
+							<div class="element" style="background-image: url(<?php the_field( 'bio_one_image' ); ?>");></div>
+						</div><!--
+						--><div class="bio">
+							<h3><?php the_field( 'bio_one_name' ); ?></h3>
+							<h4><?php the_field( 'bio_one_position' ); ?></h4>
+							<?php the_field( 'bio_one' ); ?>
+						</div>
+					</div><!--
+					--><div class="person">
+						<div class="pic">
+							<div class="element" style="background-image: url(<?php the_field( 'bio_two_image' ); ?>);"></div>
+						</div><!--
+						--><div class="bio">
+							<h3><?php the_field( 'bio_two_name' ); ?></h3>
+							<h4><?php the_field( 'bio_two_position' ); ?></h4>
+							<?php the_field( 'bio_two' ); ?>
 						</div>
 					</div>
 				</div>
@@ -68,17 +64,13 @@ get_header();
 	<div id="social">
 
 		<div class="content">
-			<div class="table">
-				<div class="cell middle">
-					<div class="container">
-						<div class="row">
-							<ul>
-								<li><a href="<?php the_field( 'twitter' ); ?>" target="_blank" title=""><i class="fab fa-twitter"></i></a></li>
-								<li><a href="<?php the_field( 'linkedin' ); ?>" target="_blank" title=""><i class="fab fa-linkedin-in"></i></a></li>
-								<li><a href="<?php the_field( 'instagram' ); ?>" target="_blank" title=""><i class="fab fa-instagram"></i></a></li>
-							</ul>
-						</div>
-					</div>
+			<div class="container">
+				<div class="row">
+					<ul>
+						<li><a href="<?php the_field( 'twitter' ); ?>" target="_blank" title=""><i class="fab fa-twitter"></i></a></li>
+						<li><a href="<?php the_field( 'linkedin' ); ?>" target="_blank" title=""><i class="fab fa-linkedin-in"></i></a></li>
+						<li><a href="<?php the_field( 'instagram' ); ?>" target="_blank" title=""><i class="fab fa-instagram"></i></a></li>
+					</ul>
 				</div>
 			</div>
 		</div>
@@ -91,14 +83,8 @@ get_header();
 		<div class="content">
 			<div class="container">
 				<div class="row">
-					<article class="columns two hide">
-						&nbsp;
-					</article>
-					<article class="columns eight">
+					<article class="columns twelve">
 						<?php echo do_shortcode( '[gravityform id="2" title="false" description="false" ajax="true"]' ); ?>
-					</article>
-					<article class="columns two hide">
-						&nbsp;
 					</article>
 				</div>
 			<!-- </div> -->
