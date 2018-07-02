35f0b11e3bdb81ec658d151052af42585717c1ee
Martin Greenwood <martin@wearewbeard.com>
2018-07-02 11:50:59 +0100

    fix

--- header.php
+++ header.php
@@ -28,7 +28,7 @@
 		<div class="box">
 			<div class="content">
 				<h3>BetaDen UK has now officially launched!</h3>
-				<p>Applications are open and we can’t wait to hear from you, the deadline for applications is the 31st of July ready to launch our first ever cohort this autumn.</p>
+				<p>Applications are open and we can’t wait to hear from you, the deadline for applications is <strong>Friday 17th August 2018</strong> ready to launch our first ever cohort this autumn.</p>
 				<a href="<?php echo home_url( '/apply' ); ?>" title="Apply online">Apply Now</a>
 				<a href="#" class="close" title="Close">Close</a>
 			</div>
